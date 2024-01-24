205b333f445592c84eb2d572b93ee06819d514e9
Pranit Dhage <pranitdhage92@gmail.com>
2024-01-24 19:13:31 +0530

    Logger setup

--- src/test/java/com/app/ECommerceApplicationTests.java
+++ src/test/java/com/app/ECommerceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.app;
 
+import com.web.ecomm.app.ECommerceWebApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ECommerceApplication.class)
+@SpringBootTest(classes = ECommerceWebApplication.class)
 class ECommerceApplicationTests {
 
 	@Test
