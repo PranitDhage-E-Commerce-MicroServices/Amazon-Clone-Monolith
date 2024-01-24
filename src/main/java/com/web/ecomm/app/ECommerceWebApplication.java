b70e9f55d008b2f0655706107fc67b053edfb952
Pranit Dhage <pranitdhage92@gmail.com>
2024-01-24 19:06:48 +0530

    Logger setup

--- src/main/java/com/web/ecomm/app/ECommerceWebApplication.java
+++ src/main/java/com/web/ecomm/app/ECommerceWebApplication.java
@@ -4,18 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-
 @Slf4j
 @SpringBootApplication
 public class ECommerceWebApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Long start = LocalDate.now().toEpochDay();
-		SpringApplication.run(ECommerceWebApplication.class, args);
+        Long start = System.currentTimeMillis();
+        SpringApplication.run(ECommerceWebApplication.class, args);
 
-		Long end = LocalDate.now().toEpochDay();
-		log.info("Application Started Successfully in {} seconds", end - start);
-	}
+        Long end = System.currentTimeMillis();
+        log.info("Application Started Successfully in {} seconds", end - start);
+    }
 }
